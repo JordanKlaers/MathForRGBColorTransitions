110e267e7cc1cd9b26df844b372ac90026995103
Jordan Klaers <jordanklaers@Lizs-MacBook-Pro.local>
2017-07-17 23:24:38 -0600

    new math wrong, old math still there

--- mathForTransitionIncrements.js
+++ mathForTransitionIncrements.js
@@ -1,55 +1,94 @@
-var a = 182
+$(document).ready(function(){
+  var a = 182
 var b = 97
 var c = 0;
 var rONE = 0;
 var rTWO = 182
 // rgb(0, 97, 255)
 // rgb(182, 0, 255)
-var increments = 100;
+var oneTransition = {}
+var increments = 100; //100 is 1 color per second at 10 ms updates
 
-function colorValueDifference(firstColor, secondColor){                //expects two objects object ColorOne{r: ,g: ,b} and ColorTwo(r: ,g: ,b: )
+
+
+
+function TransitionBetweenOneColor(firstColor, secondColor, increment, incfunc){                //expects two objects object ColorOne{r: ,g: ,b} and ColorTwo(r: ,g: ,b: )
+  // console.log(firstColor, secondColor, increment, incfunc);
   var valDif = {}
-  valDif[r] = Math.abs(firstColor.r - secondColor.r)
-  valDif[g] = Math.abs(firstColor.g - secondColor.g)
-  valDif[b] = Math.abs(firstColor.b - secondColor.b)
+  var inc = {}
+  valDif['r'] = Math.abs(firstColor.r - secondColor.r)
+  valDif['g'] = Math.abs(firstColor.g - secondColor.g)
+  valDif['b'] = Math.abs(firstColor.b - secondColor.b)
+  inc['r'] = valDif.r/increment
+  inc['g'] = valDif.g/increment
+  inc['b'] = valDif.b/increment
+  for (let key in valDif){
+    incfunc(firstColor[key], secondColor[key], inc[key], key)
+  }
 }
 
 
-function outputB(Bvalue, Zincrement){
-  return Bvalue/Zincrement;
-}
-
-function outputA(Avalue, Zincrement){
-  return Avalue/Zincrement;
-}
-
-function outputC(Cvalue, Zincrement){
-  return Cvalue/Zincrement;
-}
-
-
-function compare(Avalue, Bvalue, Cvalue, increments, one, two){
-
-   let  opb = outputB(Bvalue, increments)
-   let opa = outputA(Avalue, increments)
-   let opc = outputC(Cvalue, increments)
-   console.log("at new color every 1/2 second with 10ms update");
-   console.log("outputR- ", opa);
-   console.log("outputG- ", opb);
-   console.log("outputB- ", opc);
-   inc(one,two,opa)
-}
-
-
-function inc(one,two, inc){
+function incfunc(one,two, inc, color){
   let array = [];
   while(one<two){
     one = Math.floor(one+inc)
     array.push(one)
 
   }
-  console.log(array.length);
+  oneTransition[color] = array;
+  console.log("the money", oneTransition);
 }
 
+TransitionBetweenOneColor({r:0, g:97, b:255}, {r:182, g:0, b:255}, 50, incfunc)
+})
 
-compare(a,b,c,increments, rONE, rTWO);
+
+// 
+//
+// var a = 182
+// var b = 97
+// var c = 0;
+// var rONE = 0;
+// var rTWO = 182
+// // rgb(0, 97, 255)
+// // rgb(182, 0, 255)
+// var increments = 100;
+//
+// function outputB(Bvalue, Zincrement){
+//   return Bvalue/Zincrement;
+// }
+//
+// function outputA(Avalue, Zincrement){
+//   return Avalue/Zincrement;
+// }
+//
+// function outputC(Cvalue, Zincrement){
+//   return Cvalue/Zincrement;
+// }
+//
+//
+// function compare(Avalue, Bvalue, Cvalue, increments, one, two){
+//
+//    let  opb = outputB(Bvalue, increments)
+//    let opa = outputA(Avalue, increments)
+//    let opc = outputC(Cvalue, increments)
+//    console.log("at new color every 1/2 second with 10ms update");
+//    console.log("outputR- ", opa);
+//    console.log("outputG- ", opb);
+//    console.log("outputB- ", opc);
+//    inc(one,two,opa)
+// }
+//
+//
+// function inc(one,two, inc){
+//   let array = [];
+//   while(one<two){
+//     one = Math.floor(one+inc)
+//     array.push(one)
+//
+//   }
+//   console.log(array.length);
+// }
+//
+//
+// compare(a,b,c,increments, rONE, rTWO);
